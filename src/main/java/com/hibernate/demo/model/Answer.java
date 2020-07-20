78a796cf46598b0a953e0b3f002af8c44a56f0eb
junaidansari6311 <junaidansari6311@gmail.com>
2020-07-21 02:06:43 +0530

    ADDED: Persistent class Answer

--- src/main/java/com/hibernate/demo/model/Answer.java
+++ src/main/java/com/hibernate/demo/model/Answer.java
@@ -1,4 +1,34 @@
 package com.hibernate.demo.model;
 
+import java.util.List;
+
 public class Answer {
+    private int id;
+    private String answerName;
+    private String postedBy;
+    private List<Question> questions;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getAnswerName() {
+        return answerName;
+    }
+    public void setAnswerName(String answerName) {
+        this.answerName = answerName;
+    }
+    public String getPostedBy() {
+        return postedBy;
+    }
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
+    public List<Question> getQuestions() {
+        return questions;
+    }
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
 }
